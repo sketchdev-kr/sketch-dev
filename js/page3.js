3035a2667ce2a969ba5aba576295cd5a2ce95a07
SunMyeong Lee <actumn814@gmail.com>
2021-04-28 02:17:12 +0900

    Implemnet animation and draw canvas

---
+++ js/page3.js
@@ -0,0 +1,42 @@
+
+paper.setup("canvas");
+
+function onLoad() {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = () => {
+    if (xmlHttp.readyState !== XMLHttpRequest.DONE) return;
+
+    const res = JSON.parse(xmlHttp.responseText);
+    const canvasPaths = res.drawPaths;
+  
+    const draw = (i) => {
+      setTimeout(() => {
+        if (i === canvasPaths.length) {
+          return;
+        }
+    
+        const event = canvasPaths[i];
+        if (event.type === "down") {
+          path = new paper.Path();
+          path.strokeWidth = 5;
+          path.strokeColor = 'black';
+          path.strokeCap = 'round';
+          path.strokeJoin = 'round';
+          path.add(new paper.Point(event.x, event.y));
+        } else if (event.type === "drag") {
+          path.add(new paper.Point(event.x, event.y))
+        }
+        i+=1;
+        draw(i);
+      }, Math.random() * 40);
+    }
+    draw(0);
+  };
+  xmlHttp.open("get", "http://api.sketchdev.kr/sketches/60824cd82d0851b300d5e1d8");
+  xmlHttp.setRequestHeader("Content-Type", "application/json");
+  xmlHttp.send();
+}
+
+(function() {
+  setTimeout(onLoad, 3000);
+})()
